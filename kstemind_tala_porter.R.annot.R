f5eb91cef3c4508283e6e5c9529e90d67c4d1b26
kjarom <13.7466@stis.ac.id>
2018-08-24 14:17:10 +0700

    reworking files and folders

--- kstemind_tala_porter.R.annot.R
+++
@@ -1,3 +0,0 @@
-## kstemind_tala_porter.R - compiled by RoxygenReady, a package by @vertesy
-
-
--- others/shortcut.R
+++
@@ -1,5 +0,0 @@
-# Clear Env in RCommand
-rm(list=ls())
-
-# NewsApi.Org Key
-23c65f8247324aaea40064d91b852835
